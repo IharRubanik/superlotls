623328acdf01f3371bb644032f000eee2ac9f62a
Ernest Brozinski <ernestbrozinski@gmail.com>
2022-08-15 14:12:12 +0400

    template layout

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
         waterwayText = document.querySelector('.waterway-texts'),
         waterwayTab = document.querySelectorAll('.waterway-tab');
 
+  let percentScrolled = 0,
+      num = 0,
+      numOld = 0;
+
   waterwayTabs.forEach((el,i) => el.addEventListener('click', function(e) {
     e.preventDefault();
 
@@ -13,34 +17,58 @@ document.addEventListener("DOMContentLoaded", function() {
     waterwayTab[i].classList.add('active');
 
     const range = document.querySelector(`#waterway-text-${i+1}`).offsetTop - waterwayText.offsetTop - 1;
+
+    // x = (i === 0) ? 0 :
+    //     (i === 1) ? 16 :
+    //     (i === 2) ? 32 :
+    //     (i === 3) ? 48 : 48;
+
+    // console.log('x: ',x)
+
+    // window.scrollTo({
+    //   top: waterwaySection.offsetTop + waterwaySection.clientHeight/100 * x,
+    // })
+
+    // window.scrollTo({
+    //   top: 10
+    // });
+
     waterwayText.scrollTo({
       top: range,
       behavior: "smooth"
     });
 
   }))
-
-
-  let numOld = 0;
+  
 
   window.addEventListener('scroll', function() {
 
-    let percentScrolled =  (-1 * waterwaySection.getBoundingClientRect().y/ (waterwaySection.clientHeight/100));
+    percentScrolled = (-1 * waterwaySection.getBoundingClientRect().y/ (waterwaySection.clientHeight/100));
 
-    // console.log(percentScrolled)
+    num = (percentScrolled < 16) ? 0 :
+          (percentScrolled < 32) ? 1 :
+          (percentScrolled < 48) ? 2 :
+          (percentScrolled < 64) ? 3 : 3;
 
-    let num = (percentScrolled < 25) ? 0 :
-              (percentScrolled < 50) ? 1 :
-              (percentScrolled < 67) ? 2 :
-              (percentScrolled < 67) ? 3 : 3;
+    console.log('num: ', num)
     
-
     if (num !== numOld) {
       numOld = num;
-      waterwayTabs[num].click()
+      waterwayTabs[num].click();
     }
 
   })
 
+
+  const accordionItem = document.querySelectorAll('.accordion-item'),
+        accordionTitle = document.querySelectorAll('.accordion-title'),
+        accordionContent = document.querySelectorAll('.accordion-content');
+
+  accordionTitle.forEach((el, i) => el.addEventListener('click', function(e) {
+    e.preventDefault();
+    this.classList.toggle('show');
+    accordionItem[i].classList.toggle('show');
+    accordionContent[i].classList.toggle('show');
+  }))
 
 });
